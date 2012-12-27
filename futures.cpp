2fa33f4b08f50e38fdfbfc643f2e5fbe0e001d36
Fabrice Leal <fabrice.leal.ch@gmail.com>
2012-12-27 12:24:47 -0800

    async more clear. std::launch::async

--- futures.cpp
+++ futures.cpp
@@ -12,10 +12,50 @@ int calc_meaning_of_life()
   return 12345;
 }
 
+int calc_meaning_of_life_async()
+{
+  cout << "* Starting async calculation ..." << endl;
+  this_thread::sleep_for(chrono::milliseconds(1000));
+  cout << "* Calculation async ended ..." << endl;
+  return 12345;
+}
+
+int calc_meaning_of_life_deferred()
+{
+  cout << "* Starting deferred calculation ..." << endl;
+  this_thread::sleep_for(chrono::milliseconds(1000));
+  cout << "* Calculation deferred ended ..." << endl;
+  return 12345;
+}
+
+int calc_meaning_of_life_dont_care()
+{
+  cout << "* Starting calculation dont care ..." << endl;
+  this_thread::sleep_for(chrono::milliseconds(1000));
+  cout << "* Calculation dont care ended ..." << endl;
+  return 12345;
+}
+
+
 int main(int argc, char** argv)
 {
   future<int> the_answer = async(calc_meaning_of_life);
-  cout << "Async was launched..." << endl;
-  cout << "The meaning of life is " << the_answer.get() << endl;
+  future<int> the_answer_async = async(std::launch::async, calc_meaning_of_life_async);
+
+  // My compiler doesnt support std::launch::deferred. Cant test
+
+  // async - run on own thread
+  // deferred - wait until get is called
+  // async | deferred - chosen by implementation
+
+  //future<int> the_answer_deferred = async(std::launch::deferred, calc_meaning_of_life_deferred);
+  //future<int> the_answer_dont_care = async(std::launch::async | std::launch::deferred, calc_meaning_of_life_dont_care);
+
+  cout << "Asyncs were launched..." << endl;
+  cout << "The meaning of life is " << the_answer.get() << endl; 
+  cout << "I want an async answer: " << the_answer_async.get() << endl;
+  //cout << "I want a deferred answer: " << the_answer_deferred.get() << endl;
+  //cout << "I want a dont care answer: " << the_answer_dont_care.get() << endl;
+
   return 0;
 }
